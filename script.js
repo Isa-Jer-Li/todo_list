c6aa41c688286388fb8e691f62a6950e5b61dbcc
Isabelle Link <103222174+Isa-Jer-Li@users.noreply.github.com>
2022-05-23 17:51:06 -0500

    styling

---
+++ script.js
@@ -0,0 +1,17 @@
+let addTodoButton = document.getElementById('addTodo');
+let todoContainer = document.getElementById('todoContainer');
+let inputFeild = document.getElementById('inputFeild');
+
+addTodoButton.addEventListener('click', function(){
+	var paragraph = document.createElement('p');
+	paragraph.classList.add('paragraph-styling');
+	paragraph.innerText = inputFeild.value;
+	todoContainer.appendChild(paragraph);
+	inputFeild.value = "";
+	paragraph.addEventListener('click', function(){
+		paragraph.style.textDecoration = "line-through"
+	})
+	paragraph.addEventListener('dblclick', function(){
+		todoContainer.removeChid(paragraph);
+	})
+})
